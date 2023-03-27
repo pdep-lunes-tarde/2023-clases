aa7997fd65804a5030ff0d44289ce40a6066e957
juanFdS <juan9794@gmail.com>
2023-03-27 15:38:07 -0300

    Implementado esBisiesto

--- clase-01/intro/src/Library.hs
+++ clase-01/intro/src/Library.hs
@@ -2,7 +2,14 @@ module Library where
 import PdePreludat
 
 -- Ejercicio: año bisiesto
+-- REPL
+-- Read
+-- Evaluate
+-- Print
+-- Loop
+
+esBisiesto :: Number -> Bool
+esBisiesto año =
+    (mod año 4 == 0 && mod año 100 /= 0) || mod año 400 == 0
 -- Un año es bisiesto si es divisible por 400 o
 -- si es divisible por 4 pero no por 100.
-
-esBisiesto = implementame
