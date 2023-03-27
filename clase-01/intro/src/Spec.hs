b54a946344b463246e02b95b9365231cfbf570b3
juanFdS <juan9794@gmail.com>
2023-03-26 23:48:12 -0300

    Agregados tests para el ejercicio de año bisiesto

--- clase-01/intro/src/Spec.hs
+++ clase-01/intro/src/Spec.hs
@@ -4,4 +4,10 @@ import Library
 import Test.Hspec
 
 correrTests :: IO ()
-correrTests = mempty
+correrTests = hspec $ do
+    it "un año divisible por 400 siempre es bisiesto" $ do
+        esBisiesto 2000 `shouldBe` True
+    it "un año divisible por 4 pero no por 100 es bisiesto" $ do
+        esBisiesto 2004 `shouldBe` True
+    it "un año divisible por 4 y por 100 pero no por 400 no es bisiesto" $ do
+        esBisiesto 2100 `shouldBe` False
