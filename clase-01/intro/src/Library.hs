210c2e474f53594ea020cee9dc469bd1c338290e
juanFdS <juan9794@gmail.com>
2023-03-26 23:35:09 -0300

    Sacado codigo que no iba de la clase 1

--- clase-01/intro/src/Library.hs
+++ clase-01/intro/src/Library.hs
@@ -1,9 +1,6 @@
 module Library where
 import PdePreludat
 
--- REPL / Intérprete
--- Read. Evaluate. Print. Loop
-
 -- Ejercicio: año bisiesto
 -- Un año es bisiesto si es divisible por 400 o
 -- si es divisible por 4 pero no por 100.
