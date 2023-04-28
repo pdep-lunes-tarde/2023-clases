4b76cb9f73d5022f8b68cebdd5ed33ded88009c1
juanFdS <juan9794@gmail.com>
2023-04-28 10:19:50 -0300

    Codigo clase 5

--- clase-05/src/Library.hs
+++ clase-05/src/Library.hs
@@ -14,7 +14,7 @@ data Pokemon = Pokemon {
     } deriving(Eq, Show)
 
 entrenador :: Entrenador
-entrenador = Entrenador 10 [pikachu, charmander]
+entrenador = Entrenador 10 [pikachu, charmander, squirtle]
 
 pikachu :: Pokemon
 pikachu = Pokemon Electrico 0
@@ -29,36 +29,98 @@ squirtle :: Pokemon
 squirtle = Pokemon Agua 1000
 
 -- 1)a)
-esDeTipoElectrico = implementame
-esDeTipoFuego = implementame
-esDeTipoAgua = implementame
+esDeTipoElectrico :: Pokemon -> Bool
+esDeTipoElectrico = esDeTipo Electrico
+
+esDeTipoFuego :: Pokemon -> Bool
+esDeTipoFuego = esDeTipo Fuego
+
+esDeTipoAgua :: Pokemon -> Bool
+esDeTipoAgua = esDeTipo Agua
 
 --  b)
-esFuerte = implementame
+esFuerte pokemon = experiencia pokemon >= 1000
 
 -- 2) a) 
-losFuertes = implementame
+losFuertes :: Entrenador -> [Pokemon]
+losFuertes entrenador =
+    filter esFuerte (pokemons entrenador)
+
 
 -- b)
-losDeTipo = implementame
+losDeTipo :: Tipo -> Entrenador -> [Pokemon]
+losDeTipo unTipo entrenador =
+    pokemonsDeTipo unTipo (pokemons entrenador)
 
+esDeTipo :: Tipo -> Pokemon -> Bool
+esDeTipo unTipo pokemon = 
+    tipo pokemon == unTipo
+
+pokemonsDeTipo :: Tipo -> [Pokemon] -> [Pokemon]
+pokemonsDeTipo unTipo pokemones =
+    filter (\pokemon -> esDeTipo unTipo pokemon) pokemones
+
+-- pokemonsDeTipo unTipo [] = []
+-- pokemonsDeTipo unTipo (pokemon:resto)
+--     | coincidenTipos unTipo pokemon =
+--         pokemon : pokemonsDeTipo unTipo resto
+--     | otherwise = pokemonsDeTipo unTipo resto
+
+-- pokemonQueCumplen
+--     :: (Pokemon -> Bool) -> [Pokemon] -> [Pokemon]
+-- pokemonQueCumplen condicion [] = []
+-- pokemonQueCumplen condicion (pokemon:pokemons)
+--     | condicion pokemon =
+--         pokemon : pokemonQueCumplen condicion pokemons
+--     | otherwise = pokemonQueCumplen condicion pokemons
 
 -- 3)
 
-entrenar = implementame
+entrenar :: Number -> Pokemon -> Pokemon
+entrenar minutos pokemon = aumentarExperiencia (minutos * 2) pokemon
+
+aumentarExperiencia :: Number -> Pokemon -> Pokemon
+aumentarExperiencia cantidad (Pokemon tipo experiencia) = Pokemon tipo (experiencia + cantidad)
 
 -- 4)
 
-entrenarEquipo = implementame
+entrenarEquipo :: Number -> Entrenador -> Entrenador
+entrenarEquipo minutos (Entrenador pokebolas pokemons) =
+    Entrenador pokebolas (map (\pokemon -> entrenar minutos pokemon) pokemons)
 
 -- 5)
 
 puedeSerLiderDeGimnasio = implementame
 
+tieneAlMenosUnoFuerte :: Entrenador -> Bool
+tieneAlMenosUnoFuerte entrenador = any esFuerte (pokemons entrenador)
+
+todosSonDelMismoTipo :: Entrenador -> Bool
+todosSonDelMismoTipo entrenador =
+    all (\pokemon -> tienenMismoTipo (primerPokemon entrenador) pokemon)
+        (pokemons entrenador)
+
+tienenMismoTipo :: Pokemon -> Pokemon -> Bool
+tienenMismoTipo unPokemon otroPokemon = tipo unPokemon == tipo otroPokemon
+
+primerPokemon :: Entrenador -> Pokemon
+primerPokemon entrenador = head (pokemons entrenador)
+
 -- 6)
 
-atrapar = implementame
+atrapar :: Entrenador -> Pokemon -> Entrenador
+atrapar entrenador pokemon
+    | tienePokebolas entrenador = agregarAlEquipo entrenador pokemon
+    | otherwise = entrenador
+
+tienePokebolas :: Entrenador -> Bool
+tienePokebolas (Entrenador pokebolas _) = pokebolas > 0
+
+agregarAlEquipo :: Entrenador -> Pokemon -> Entrenador
+agregarAlEquipo (Entrenador pokebolas pokemons) pokemon =
+    Entrenador pokebolas (pokemon : pokemons)
 
 -- 7)
 
-atraparHorda = implementame
+atraparHorda :: Entrenador -> [Pokemon] -> Entrenador
+atraparHorda entrenador pokemons = foldl atrapar entrenador pokemons
