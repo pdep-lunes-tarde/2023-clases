aa4e01bf2f9f4fc125ae025fa6127c0d99d0fa1a
juanFdS <juan9794@gmail.com>
2023-05-15 15:54:13 -0300

    Codigo clase 7

--- clase-07/src/Library.hs
+++ clase-07/src/Library.hs
@@ -1,26 +1,54 @@
+{-# LANGUAGE BangPatterns #-}
 module Library where
 import PdePreludat
 
+constLazy :: a -> b -> a
+constLazy x y = x
+
+-- constLazy (trace "evaluo 5" 5)
+--           (trace "evaluo 10" 10)
+-- -- es equivalente a:
+-- (trace "evaluo 5" 5)
+
+
+constEager :: a -> b -> a
+constEager !x !y = x
+
+
 unos :: [Number]
-unos = implementame
+unos = 1 : unos
 
 repetir :: a -> [a]
-repetir valor = implementame
+repetir valor = valor : repetir valor
 
 replicar :: Number -> a -> [a]
-replicar cantidadDeVeces valor = implementame
+replicar cantidadDeVeces valor =
+    (take cantidadDeVeces . repetir) valor
 
 naturales :: [Number]
-naturales = implementame
+naturales = 1 : map (+1) naturales
+-- 1 : map (+1) naturales
+-- 1 : map (+1) (1 : map (+1) naturales)
+-- 1 : (1 + 1 : map (+1) (map (+1) naturales))
+-- 1 : 2 : (map (+1) (map (+1) naturales))
+
+-- naturales = map (+1) unos
 
 losNaturalesPares :: [Number]
-losNaturalesPares = implementame
+losNaturalesPares = 2 : map (+2) losNaturalesPares
+losNaturalesPares' :: [Number]
+losNaturalesPares' = filter even naturales
 
 losNaturalesImpares :: [Number]
-losNaturalesImpares = implementame
+losNaturalesImpares = filter odd naturales
 
 serieDeFibonacci :: [Number]
-serieDeFibonacci = implementame
+serieDeFibonacci = 0 : 1 : zipWith (+)
+                                   serieDeFibonacci
+                                   (tail serieDeFibonacci)
+-- 0 | 1 | 1 | 2 | 3
+--   | 0 | 1 | 1 | 2 | 3
 
-elPrimero :: p1 -> p2 -> p1
-elPrimero a b = a
+-- 0 | 1 | 1 | 2 | 3
+
+-- 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8 -> 13 -> 21
