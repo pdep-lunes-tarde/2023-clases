c0f4d31f38f0b45f8a62ac562e02c717abf5c24a
juanFdS <juan9794@gmail.com>
2023-08-07 16:16:02 -0300

    Codigo inicial clase 15

---
+++ clase-15/README.md
@@ -0,0 +1,40 @@
+# Clase 15 - Listas y findall
+
+## Material de la clase
+
+- Slides: 
+- Código: Acá :D.
+
+## ¿Que hay para la clase que viene?
+
+TP-Integrador: **Durante la semana se libera**
+
+## ¿Qué mas puedo leer, ver o practicar?
+
+### Apuntes de la cátedra
+
+- [Módulo 3: Estructuras de datos. Listas.](https://docs.google.com/document/d/1I8Xvss7LBuUjV-GGiag7C8d9wa3vUB6B37Qi4LG-ts0/edit)
+- [Módulo 4: Predicados de Orden Superior. Findall](https://docs.google.com/document/d/1GGair_St5yWvItKRZH-FY_X2CdDREr60TrsV0zSiO5I/edit?usp=sharing)
+
+### Wiki de Uqbar
+
+- [Listas en Prolog](https://wiki.uqbar.org/wiki/articles/paradigma-logico---listas.html)
+
+### Práctica de Mumuki
+
+- [Lección 10: Findall](https://mumuki.io/pdep-utn/lessons/708-programacion-logica-mas-alla-del-paradigma-findall)
+- [Lección 11: Listas y Findall](https://mumuki.io/pdep-utn/lessons/709-programacion-logica-practica-listas-y-findall)
+
+### Videos
+
+- Listas y Findall por Maiu y Juan, 1.45hs
+
+[![Listas y Findall por Maiu y Juan](https://img.youtube.com/vi/0Fb6QZXG-5c/0.jpg)](https://youtu.be/0Fb6QZXG-5c "Listas y Findall por Maiu y Juan")
+
+- Práctica con parcial por Maiu + Feche + Nahue + Nico B + Juan DS.
+
+[![Práctica con parcial: Las casas de hogwarts](https://img.youtube.com/vi/FysNBkzJQ8o/0.jpg)](https://youtu.be/FysNBkzJQ8o "Práctica con parcial: Las casas de hogwarts")
+
+- Backtracking: Recorriendo un laberinto con prolog
+[![Backtracking: Recorriendo un laberinto con prolog](https://img.youtube.com/vi/EgjdNl6eQvI/0.jpg)](https://youtu.be/EgjdNl6eQvI "Backtracking: Recorriendo un laberinto con prolog")
+
---
+++ clase-15/teg.pl
@@ -0,0 +1,132 @@
+jugador(amarillo).
+jugador(magenta).
+jugador(azul).
+jugador(verde).
+jugador(negro).
+jugador(rojo).
+
+continente(americaDelSur).
+continente(americaDelNorte).
+continente(asia).
+continente(oceania).
+continente(europa).
+continente(africa).
+
+estaEn(americaDelSur, argentina).
+estaEn(americaDelSur, brasil).
+estaEn(americaDelSur, chile).
+estaEn(americaDelSur, uruguay).
+estaEn(americaDelSur, peru).
+estaEn(americaDelSur, colombia).
+
+estaEn(americaDelNorte, alaska).
+estaEn(americaDelNorte, yukon).
+estaEn(americaDelNorte, canada).
+estaEn(americaDelNorte, oregon).
+estaEn(americaDelNorte, nuevaYork).
+estaEn(americaDelNorte, terranova).
+estaEn(americaDelNorte, labrador).
+estaEn(americaDelNorte, mexico).
+estaEn(americaDelNorte, groenlandia).
+estaEn(americaDelNorte, california).
+
+estaEn(europa, islandia).
+estaEn(europa, granBretania).
+estaEn(europa, suecia).
+estaEn(europa, rusia).
+estaEn(europa, polonia).
+estaEn(europa, alemania).
+estaEn(europa, italia).
+estaEn(europa, francia).
+estaEn(europa, espania).
+
+estaEn(asia, aral).
+estaEn(asia, tartaria).
+estaEn(asia, taimir).
+estaEn(asia, kamchatka).
+estaEn(asia, china).
+estaEn(asia, siberia).
+estaEn(asia, japon).
+estaEn(asia, mongolia).
+estaEn(asia, iran).
+estaEn(asia, gobi).
+estaEn(asia, india).
+estaEn(asia, malasia).
+estaEn(asia, turquia).
+estaEn(asia, israel).
+estaEn(asia, arabia).
+
+estaEn(oceania, australia).
+estaEn(oceania, sumatra).
+estaEn(oceania, java).
+estaEn(oceania, borneo).
+
+estaEn(africa, sahara).
+estaEn(africa, egipto).
+estaEn(africa, etiopia).
+estaEn(africa, madagascar).
+estaEn(africa, sudafrica).
+estaEn(africa, zaire).
+
+ocupa(aral, magenta).
+
+ocupa(alaska, amarillo).
+ocupa(yukon, amarillo).
+ocupa(oregon, amarillo).
+ocupa(canada, amarillo).
+ocupa(groenlandia, amarillo).
+ocupa(labrador, amarillo).
+ocupa(terranova, amarillo).
+ocupa(nuevaYork, amarillo).
+ocupa(california, amarillo).
+ocupa(mexico, amarillo).
+
+ocupa(islandia, amarillo).
+ocupa(suecia, amarillo).
+ocupa(rusia, amarillo).
+ocupa(polonia, amarillo).
+
+ocupa(turquia, amarillo).
+
+ocupa(sahara, amarillo).
+ocupa(egipto, amarillo).
+ocupa(etiopia, amarillo).
+ocupa(zaire, amarillo).
+ocupa(madagascar, amarillo).
+ocupa(sudafrica, amarillo).
+
+ocupa(argentina, azul).
+ocupa(chile, azul).
+ocupa(uruguay, azul).
+
+ocupa(china, azul).
+ocupa(japon, azul).
+ocupa(iran, azul).
+ocupa(india, azul).
+ocupa(malasia, azul).
+ocupa(gobi, azul).
+
+ocupa(australia, azul).
+ocupa(borneo, azul).
+ocupa(java, azul).
+ocupa(sumatra, azul).
+
+ocupa(israel, negro).
+ocupa(arabia, negro).
+
+ocupa(mongolia, verde).
+ocupa(taimir, verde).
+ocupa(tartaria, verde).
+ocupa(siberia, verde).
+ocupa(kamchatka, verde).
+
+ocupa(italia, verde).
+
+ocupa(brasil, verde).
+ocupa(colombia, verde).
+ocupa(peru, verde).
+
+ocupa(francia, rojo).
+ocupa(granBretania, rojo).
+ocupa(espania, rojo).
+ocupa(alemania, rojo).
